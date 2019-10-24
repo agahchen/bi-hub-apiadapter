4195918d4942c7cda933eef27523160216b592bc
echo liang <hhliang66@hotmail.com>
2019-10-24 15:13:55 -0700

    Removed wrong import

--- src/main/java/bcgov/bi/bihubadapter/controller/BiHubApiAdapterRestController.java
+++ src/main/java/bcgov/bi/bihubadapter/controller/BiHubApiAdapterRestController.java
@@ -12,11 +12,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import bcgov.jh.etk.jhetkcommon.model.PathConst;
 
 @RestController
 public class BiHubApiAdapterRestController {
