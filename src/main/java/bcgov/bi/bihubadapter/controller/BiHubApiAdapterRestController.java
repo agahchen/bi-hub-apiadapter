23dfdbeb0ccfbc88f989fb62bdedf0ded32b720f
echo liang <hhliang66@hotmail.com>
2019-10-24 13:36:15 -0700

    fixed the header object

--- src/main/java/bcgov/bi/bihubadapter/controller/BiHubApiAdapterRestController.java
+++ src/main/java/bcgov/bi/bihubadapter/controller/BiHubApiAdapterRestController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +45,7 @@ public class BiHubApiAdapterRestController {
 		//send request to Hub api to request eTK event KPI
 	    try {
 	    	logger.info("Send request to Hub API {} to request eTK event KPI", url);
-	    	response = restTemplate.exchange(url, HttpMethod.GET, null, String.class);
+	    	response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(new HttpHeaders()), String.class);
 	    	if (HttpStatus.OK.equals(response.getStatusCode()) || HttpStatus.CREATED.equals(response.getStatusCode())) {
 	    		logger.info(response.getBody());
 			}
